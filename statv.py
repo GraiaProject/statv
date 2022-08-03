63c217a73f0d5643d6b9667c55da548c80a2008d
BlueGlassBlock <blueglassblock@outlook.com>
2022-08-03 16:57:09 +0800

    feat: add on_update

--- statv.py
+++ statv.py
@@ -12,9 +12,16 @@ EllipsisType = type(...)
 
 T = TypeVar("T")
 
+Statv_T = TypeVar("Statv_T", bound="Statv", contravariant=True)
+
 
 class StatsValidator(Protocol[T]):
     def __call__(self, stats: Stats[T], past: T, current: T) -> T:
+        ...
+
+
+class UpdateMonitor(Protocol[Statv_T, T]):
+    def __call__(self, statv: Statv_T, stats: Stats[T], past: T, current: T) -> Any:
         ...
 
 
@@ -47,9 +54,7 @@ class Stats(Generic[T]):
     def __get__(self, instance: Statv, owner: type[Statv]) -> T:
         ...
 
-    def __get__(
-        self, instance: Statv | None, owner: type[Statv] | None = None
-    ) -> T | Stats:
+    def __get__(self, instance: Statv | None, owner: type[Statv] | None = None) -> T | Stats:
         if instance is None:
             return self
 
@@ -59,8 +64,12 @@ class Stats(Generic[T]):
         return instance._stats[self.id]
 
     def __set__(self, instance: Statv, value: T) -> None:
+        past_value: T = instance._stats[self.id]
         if self._validator:
-            value = self._validator(self, instance._stats[self.id], value)
+            value = self._validator(self, past_value, value)
+        if past_value != value:
+            for monitor in instance._monitors.get(self.id, []):
+                monitor(instance, self, past_value, value)
         instance._stats[self.id] = value
 
         for ftr in instance._waiters:
@@ -74,15 +83,14 @@ class Stats(Generic[T]):
         return validator
 
 
-def stats(
-    id: str, *, default: Any = ..., default_factory: Callable[[], Any] | None = None
-) -> Any:  # sourcery skip: instance-method-first-arg-name
+def stats(id: str, *, default: Any = ..., default_factory: Callable[[], Any] | None = None) -> Stats[Any]:
     return Stats(id, default=default, default_factory=default_factory)
 
 
 class Statv:
     _waiters: deque[asyncio.Future]
     _stats: dict[str, Any]
+    _monitors: dict[str, list[UpdateMonitor[Self, Any]]]
 
     def __init__(self, *, init_stats: dict[str, Any] | None = None):
         self._waiters = deque()
@@ -102,6 +110,13 @@ class Statv:
     def defined_stats(cls) -> list[Stats]:
         return [v for _, v in inspect.getmembers(cls, lambda x: isinstance(x, Stats))]
 
+    def on_update(self, stats: Stats[T]) -> Callable[[UpdateMonitor[Self, T]], UpdateMonitor[Self, T]]:
+        def inner(func: UpdateMonitor[Self, T]) -> UpdateMonitor[Self, T]:
+            self._monitors.setdefault(stats.id, []).append(func)
+            return func
+
+        return inner
+
     @property
     def available(self) -> bool:
         return True
@@ -109,15 +124,20 @@ class Statv:
     def update_multi(self, mapping: dict[Stats[T], T]) -> None:
         stats = self.defined_stats()
         if not all(isinstance(k, Stats) and k in stats for k in mapping):
-            raise TypeError(
-                f"invalid ownership of Stats definition for {self.__class__.__name__}"
-            )
+            raise TypeError(f"invalid ownership of Stats definition for {self.__class__.__name__}")
 
         for stat, value in mapping.items():
+            past_value: T = self._stats[stat.id]
+            if stat._validator:
+                value = stat._validator(stat, past_value, value)
+            if past_value != value:
+                for monitor in self._monitors.get(stat.id, []):
+                    monitor(self, stat, past_value, value)
             self._stats[stat.id] = value
 
         for ftr in self._waiters:
-            ftr.set_result(self)
+            if not ftr.done() and not ftr.cancelled():
+                ftr.set_result(self)
 
     async def wait_for_update(self: Self) -> tuple[Self, Self]:
         waiter = asyncio.Future()
